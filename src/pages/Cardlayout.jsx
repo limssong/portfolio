5a10581560f356e9eb3d124aa4e1b89e4e37247e
limssong <limssong0411@gmail.com>
2024-12-15 13:43:25 +0900

    수정

--- src/pages/Cardlayout.jsx
+++ src/pages/Cardlayout.jsx
@@ -4,7 +4,12 @@ const Cardlayout = (props) => {
     <li class="portfolio-card">
       <a href={portfolioData.url} alt={portfolioData.title} target={portfolioData.target}>
         <div class="portfolio-thumb"><img src={portfolioData.imgUrl} alt={portfolioData.title} /></div>
-        
+        <div class="portfolio-info">
+          <div class="portfolio-title">{portfolioData.title}</div>
+          <div class="portfolio-desc">{portfolioData.desc}</div>
+          <div class="portfolio-skill">{portfolioData.skill}</div>
+          <div class="portfolio-date">{portfolioData.date}</div>
+        </div>
       </a>
     </li>
   )
