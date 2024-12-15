46ed586909c88202f72b69f0423c445aaf5860ce
limssong <limssong0411@gmail.com>
2024-12-15 13:31:59 +0900

    Update Cardlayout.jsx

--- src/pages/Cardlayout.jsx
+++ src/pages/Cardlayout.jsx
@@ -4,11 +4,7 @@ const Cardlayout = (props) => {
     <li class="portfolio-card">
       <a href={portfolioData.url} alt={portfolioData.title} target={portfolioData.target}>
         <div class="portfolio-thumb"><img src={portfolioData.imgUrl} alt={portfolioData.title} /></div>
-        <div class="portfolio-info">
-          <div class="portfolio-title">{portfolioData.title}</div>
-          <div class="portfolio-desc">{portfolioData.desc}</div>
-          <div class="portfolio-skill">{portfolioData.skill} test</div>
-        </div>
+        
       </a>
     </li>
   )
